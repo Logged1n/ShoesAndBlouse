13427f02b26656d935a389f7d4f427df64334582
JacobWick <134642281+JacobWick@users.noreply.github.com>
2024-04-03 17:12:22 +0200

    Corrections to Repositories and their interfaces

--- ShoesAndBlouse.Domain/Interfaces/IReviewRepository.cs
+++ ShoesAndBlouse.Domain/Interfaces/IReviewRepository.cs
@@ -4,8 +4,8 @@ namespace ShoesAndBlouse.Domain.Interfaces;
 
 public interface IReviewRepository
 {
-    Task<ICollection<Domain.Entities.Review>> GetAll();
-    Task<Review?> GetReviewById(int reviewId);
+    Task<ICollection<Domain.Entities.Review>> GetAll(CancellationToken cancellationToken=default);
+    Task<Review?> GetReviewById(int reviewId, CancellationToken cancellationToken=default);
     Task<Review> CreateReview(Review toCreate, CancellationToken cancellationToken=default);
     Task<Review?> UpdateReview(Review toUpdate, CancellationToken cancellationToken=default);
     Task<Review?> DeleteReview(int reviewId, CancellationToken cancellationToken=default);
--- ShoesAndBlouse.Domain/Interfaces/IUserRepository.cs
+++ ShoesAndBlouse.Domain/Interfaces/IUserRepository.cs
@@ -4,8 +4,8 @@ namespace ShoesAndBlouse.Domain.Interfaces
 {
     public interface IUserRepository
     {
-        Task<ICollection<User>> GetAll();
-        Task<User?> GetUserById(int userId);
+        Task<ICollection<User>> GetAll(CancellationToken cancellationToken=default);
+        Task<User?> GetUserById(int userId, CancellationToken cancellationToken=default);
         Task<User> CreateUser(User toCreate, CancellationToken cancellationToken = default);
         Task<User?> UpdateUser(User toUpdate, CancellationToken cancellationToken = default);
         Task<User?> DeleteUser(int userId, CancellationToken cancellationToken=default);
