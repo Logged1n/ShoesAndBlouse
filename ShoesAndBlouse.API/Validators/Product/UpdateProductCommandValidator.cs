6eed8b1ab1623f9a18d9960cd10048fc4a5e5a76
Lukasz Halicki <uhalicki@gmail.com>
2024-04-06 21:22:31 +0200

    Corrected docker services; Added first FluentValidators for Product Commands in ProductController.cs

---
+++ ShoesAndBlouse.API/Validators/Product/UpdateProductCommandValidator.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using ShoesAndBlouse.API.Validators.ValueObjects;
+using ShoesAndBlouse.Application.Products.Commands;
+
+namespace ShoesAndBlouse.API.Validators.Product;
+
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.ProductId)
+            .GreaterThan(0).WithMessage("Product ID must be greater than zero.");
+
+        When(x => !string.IsNullOrEmpty(x.Name), () =>
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Product name cannot be empty.")
+                .MaximumLength(100).WithMessage("Product name cannot exceed 100 characters.");
+        });
+
+        When(x => !string.IsNullOrEmpty(x.Description), () =>
+        {
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Product description cannot be empty.")
+                .MaximumLength(1000).WithMessage("Product description cannot exceed 1000 characters.");
+        });
+
+        When(x => x.Price != null, () =>
+        {
+            RuleFor(x => x.Price)
+                .NotNull().WithMessage("Product price cannot be null.")
+                .SetValidator(
+                    new MoneyValidator()!); // Validation for the Price property using the appropriate validator
+        });
+
+        When(x => x.Category != null && x.Category.Count != 0, () =>
+        {
+            RuleFor(x => x.Category)
+                .Must(c => c != null && c.All(cat => !string.IsNullOrEmpty(cat))).WithMessage("Category names cannot be empty.");
+        });
+
+        When(x => !string.IsNullOrEmpty(x.PhotoPath), () =>
+        {
+            RuleFor(x => x.PhotoPath)
+                .NotEmpty().WithMessage("Product photo path cannot be empty.");
+        });
+    }
+}
