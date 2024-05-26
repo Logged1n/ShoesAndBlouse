87289d81f9f249fe8a9b5e5a6fc87792510183c0
Łukasz Halicki <uhalicki@gmail.com>
2024-05-26 21:17:49 +0200

    went back to Lists from HashSets; added admin crud sketch with MartialUI

--- shoes-and-blouse-frontend/src/app/dummy/page.tsx
+++ shoes-and-blouse-frontend/src/app/dummy/page.tsx
@@ -1,17 +1,59 @@
 "use client"
 
 import axios from "axios";
+import {useState} from "react";
+
+interface Price {
+    currency: string;
+    amount: number;
+}
+
+interface Product {
+    id: number;
+    name: string;
+    description: string;
+    price: Price;
+    categories: Record<string, string>;
+    photoUrl: string;
+}
 
 export default function Dummy() {
+    const [products, setProducts] = useState<Product[]>([]);
     const makeApiCall = async (): Promise<string> => {
-         const data = await axios.get(`backendAPI/v1/Product/GetById/1`);
+         const data = await axios.get<Product[]>(`backendAPI/v1/Product/GetALL`);
          console.log(data);
+         setProducts(data.data);
         return JSON.stringify(data);
     }
 
     return (
         <div>
             <button onClick={makeApiCall}>Call</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                    { products.length > 0 ?
+                        products.map((product) => (
+                            <tr>
+                                <td key={product.id}>
+                                    {product.id}
+                                </td>
+                                <td key={product.id}>
+                                    {product.name}
+                                </td>
+                            </tr>
+                                ))
+                                :"Fetching products..."}
+                           
+                        </tbody>
+                
+            </table>
         </div>
     );
 }
