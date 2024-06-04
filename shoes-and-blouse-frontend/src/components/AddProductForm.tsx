a1db00b7821bdc14935ae6d35b983cf3fa9894d0
JacobWick <134642281+JacobWick@users.noreply.github.com>
2024-06-04 19:25:41 +0200

    AddProductForm

--- shoes-and-blouse-frontend/src/components/AddProductForm.tsx
+++ shoes-and-blouse-frontend/src/components/AddProductForm.tsx
@@ -1,68 +1,122 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
-import { TextField, Button, FormControl } from '@mui/material';
-import { Price, Category } from '@/app/_types/api_interfaces';
-import { AddProductFormProps } from '@/app/_types/api_interfaces';
+"use client"
 
-const AddProductForm: React.FC<AddProductFormProps> = ({ onSubmit }) => {
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState<Price>({ currency: 'PLN', amount: 0 });
-    const [description, setDescription] = useState('');
-    const [image, setImage] = useState('');
-    const [categories, setCategories] = useState<string>('');
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { AddProductFormProps, Category } from "@/app/_types/api_interfaces";
+import { GetCategories } from "@/app/actions/actions";
+import { TextField, Checkbox, FormControlLabel, Button, Typography, Box, FormGroup } from "@mui/material";
 
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        const categoryArray = categories.split(',').map(category => category.trim());
-        const product = { name, price, description, image, categories: categoryArray };
-        onSubmit(product);
+export default function AddProductForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<AddProductFormProps>({});
+
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [selectedCategories, setSelectedCategories] = useState<number[]>([]);
+
+    useEffect(() => {
+        async function fetchCategories() {
+            const categories = await GetCategories();
+            console.log(categories);
+            setCategories(categories);
+        }
+        fetchCategories();
+    }, []);
+
+    const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const categoryId = parseInt(event.target.value, 10);
+        setSelectedCategories(prevSelected =>
+            event.target.checked
+                ? [...prevSelected, categoryId]
+                : prevSelected.filter(id => id !== categoryId)
+        );
     };
 
-    const handlePriceChange = (e: ChangeEvent<HTMLInputElement | { name?: string; value: unknown }>) => {
-        setPrice({ ...price, [e.target.name as string]: e.target.value as string | number });
+    const onSubmit = async (data: any) => {
+        try {
+            const productData = { ...data, categories: selectedCategories };
+            await axios.post("/backendAPI/api/v1/Product/Create", productData, {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "*/*",
+                }
+            }).then(res => console.log(res.data));
+        } catch (error) {
+            console.error('Błąd przy tworzeniu produktu: ', (error as Error).message);
+        }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <TextField label="Nazwa" value={name} onChange={(e) => setName(e.target.value)} fullWidth margin="normal" />
-            <TextField
-                label="Kwota"
-                type="number"
-                name="amount"
-                value={price.amount}
-                onChange={handlePriceChange}
-                fullWidth
-                margin="normal"
-            />
-            <TextField
-                label="Waluta"
-                name="currency"
-                value={price.currency}
-                onChange={handlePriceChange}
-                fullWidth
-                margin="normal"
-            />
-            <TextField
-                label="Opis"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                fullWidth
-                margin="normal"
-                multiline
-                rows={4}
-            />
-            <TextField label="Zdjęcie URL" value={image} onChange={(e) => setImage(e.target.value)} fullWidth margin="normal" />
-            <TextField
-                label="Kategorie (oddzielone przecinkami)"
-                value={categories}
-                onChange={(e) => setCategories(e.target.value)}
-                fullWidth
-                margin="normal"
-            />
-            <Button type="submit" variant="contained" color="primary" fullWidth>
-                Dodaj Produkt
-            </Button>
-        </form>
-    );
-};
+        <Box component="div" sx={{ maxWidth: 400, mx: "auto", mt: 4 }}>
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
+                <TextField
+                    label="Name"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    {...register("name", { required: "Wrong name" })}
+                    error={!!errors.name}
+                    helperText={errors.name ? errors.name.message : ""}
+                    placeholder="Name of a product"
+                />
 
-export default AddProductForm;
+                <TextField
+                    label="Description"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    {...register("description", { required: "Wrong description" })}
+                    error={!!errors.description}
+                    helperText={errors.description ? errors.description.message : ""}
+                    placeholder="Description of the product"
+                />
+
+                <TextField
+                    label="Currency"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    {...register("currency", { required: "Wrong currency" })}
+                    error={!!errors.currency}
+                    helperText={errors.currency ? errors.currency.message : ""}
+                    placeholder="PLN"
+                />
+
+                <TextField
+                    label="Amount"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    {...register("amount", { required: "Wrong amount" })}
+                    error={!!errors.amount}
+                    helperText={errors.amount ? errors.amount.message : ""}
+                    placeholder="100"
+                />
+
+                <Typography variant="h6" component="h3" gutterBottom>
+                    Categories
+                </Typography>
+                <FormGroup>
+                    {categories.map(category => (
+                        <FormControlLabel
+                            key={category.id}
+                            control={
+                                <Checkbox
+                                    value={category.id}
+                                    onChange={handleCheckboxChange}
+                                />
+                            }
+                            label={category.name}
+                        />
+                    ))}
+                </FormGroup>
+                <Button type="submit" variant="contained" color="primary" fullWidth>
+                    Submit
+                </Button>
+            </form>
+        </Box>
+    );
+}
