27a6d1de70112f16894462bf6c015a7cf4dfb73c
Lukasz Halicki <uhalicki@gmail.com>
2024-06-11 19:59:23 +0200

    EditCategory fixed

--- shoes-and-blouse-frontend/src/app/(adminPages)/adminPanel/categories/edit/[id]/page.tsx
+++ shoes-and-blouse-frontend/src/app/(adminPages)/adminPanel/categories/edit/[id]/page.tsx
@@ -1,18 +1,19 @@
 "use client"
 
 import {Container, Typography} from "@mui/material";
-import EditCategoryForm from "@/components/AddCategoryForm";
+import EditCategoryForm from "@/components/EditCategoryForm";
 import React from "react";
 
+
+
 export default function EditCategory({params}: {params: {id: string}}){
+
     return(
         <Container>
             <Typography variant="h4" align="center" gutterBottom>
                 Edit Category
             </Typography>
-            <EditCategoryForm params={params}>
-
-            </EditCategoryForm>
+            <EditCategoryForm id={params.id as string}/>
         </Container>
     )
 }
--- shoes-and-blouse-frontend/src/app/_types/api_interfaces.ts
+++ shoes-and-blouse-frontend/src/app/_types/api_interfaces.ts
@@ -18,7 +18,7 @@ export interface Product {
     description: string;
     price: Price;
     categories: Record<number, string>;
-    photoUrl: File;
+    photoUrl: string;
 }
 
 export interface Category {
